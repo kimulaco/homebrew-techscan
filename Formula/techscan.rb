8de5b05d78b3033c25209250d871280925045d0c
kimulaco <kimulaco@gmail.com>
2025-09-20 22:03:37 +0900

    feat: create homebrew formula

---
+++ Formula/techscan.rb
@@ -0,0 +1,30 @@
+class Techscan < Formula
+  desc "techscan is a Rust CLI tool for analyzing and visualizing technology stacks in a directory"
+  homepage "https://github.com/kimulaco/techscan"
+  license "MIT"
+
+  version "1.0.1"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/kimulaco/techscan/releases/download/v#{version}/techscan-darwin-arm64.tar.gz"
+      sha256 "adbb5f7468717be54c623333fa809d3a0616e84310c0a0f827a485824e48c243"
+    else
+      url "https://github.com/kimulaco/techscan/releases/download/v#{version}/techscan-darwin-x64.tar.gz"
+      sha256 "7a760b3c3bd0b9849df898450515a2bbd888b20c670ebf5cbe289edadb185238"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/kimulaco/techscan/releases/download/v#{version}/techscan-linux-x64.tar.gz"
+    sha256 "0358c7465422357d9dd98017b00068703409ee15966511d5711ebbe049b1a2b1"
+  end
+
+  def install
+    bin.install "techscan"
+  end
+
+  test do
+    assert_match "techscan", shell_output("#{bin}/techscan --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,28 @@
+# homebrew-techscan
+
+This is a Homebrew tap for distributing the `techscan` CLI tool. For detailed information about the tool, usage instructions, and documentation, please visit the main repository:
+
+[kimulaco/techscan](https://github.com/kimulaco/techscan)
+
+## Installation
+
+```bash
+# Add the tap and install
+brew tap kimulaco/techscan
+brew install techscan
+
+# Verify installation
+techscan --version
+
+# Scan programing languages
+techscan lang .
+```
+
+## Support
+
+- **Tool Issues**: Please report issues with the techscan tool itself to the [main repository](https://github.com/kimulaco/techscan/issues)
+- **Formula Issues**: For Homebrew-specific installation issues, create an issue in this repository
+
+## License
+
+[MIT License](LICENSE).
